e94cdf295a069a64d6af9812d0ce1c61bbd32eaa
Nguhi <nguhi.dev@gmail.com>
2025-10-25 13:33:13 +0300

    :sparkles: add time limit and concurrency to quiz game - Implement a time limit for the quiz (12 seconds) - Add concurrency using goroutines and channels - Handle quiz completion and timeout scenarios - Close file after reading problems to prevent leaks

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"time"
 )
 
 type Problem struct {
@@ -14,32 +15,35 @@ type Problem struct {
 	Answer   string
 }
 
+const quizDuration = 12
+
 var count int
 
 func main() {
-
 	problems := getProblems("problems.csv")
-	scanner := bufio.NewScanner(os.Stdin)
+	timer := time.After(quizDuration * time.Second)
 
-	fmt.Println("Starting Quiz....")
-	fmt.Println("Press Enter key to move to next Quiz Question. Good Luck!")
+	completed := make(chan bool)
 
-	for i, problem := range problems {
-		fmt.Printf("%d. %s = ", i+1, problem.Question)
+	go runQuiz(problems, completed)
 
-		scanner.Scan()
-		ans := scanner.Text()
-
-		if ans == problem.Answer {
-			count++
+	for {
+		select {
+		case <-timer:
+			fmt.Println("\nTime's up!")
+			fmt.Printf("You scored %d out of %d\n", count, len(problems))
+			os.Exit(0)
+		case <-completed:
+			fmt.Printf("You scored %d out of %d\n", count, len(problems))
+			os.Exit(0)
+		default:
 		}
 	}
-
-	fmt.Printf("You scored %d out of %d\n", count, len(problems))
 }
 
 func getProblems(filepath string) []*Problem {
 	file, err := os.Open(filepath)
+	defer file.Close()
 
 	if err != nil {
 		log.Fatal(err)
@@ -64,4 +68,24 @@ func getProblems(filepath string) []*Problem {
 	}
 
 	return problems
+}
+
+func runQuiz(problems []*Problem, done chan bool) {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Println("Starting Quiz....")
+	fmt.Println("Press Enter key to move to next Quiz Question. Good Luck!")
+
+	for i, problem := range problems {
+		fmt.Printf("%d. %s = ", i+1, problem.Question)
+
+		scanner.Scan()
+		ans := scanner.Text()
+
+		if ans == problem.Answer {
+			count++
+		}
+	}
+
+	done <- true
 }
